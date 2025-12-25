d69969196d6c7de0328ff7532fdd606ecd128dde
Leo-Mart <martin.christoffer.leo@gmail.com>
2025-12-25 21:27:44 +0100

    setup postgresql, sqlc, goose migrations

--- main.go
+++ main.go
@@ -1,22 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/Leo-Mart/go_http_server/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             database.Queries
 }
 
 func main() {
+	godotenv.Load()
+
 	const filePathRoot = "."
 	const port = "8080"
+	dbUrl := os.Getenv("DB_URL")
 	mux := http.NewServeMux()
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatalf("error opening database: %v", err)
+	}
+
+	dbQueries := database.New(db)
 
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		db:             *dbQueries,
 	}
 
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))))
